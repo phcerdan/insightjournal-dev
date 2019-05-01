d34821f8515548da1a5c31ed2b9b32be50d69c5f
Pablo Hernandez-Cerdan <pablo.hernandez.cerdan@outlook.com>
2019-05-01 15:45:29 -0400

    Improve remove_duplicatedName.py and log results

--- remove_duplicatedName.py
+++ remove_duplicatedName.py
@@ -5,6 +5,16 @@ import os
 import hashlib
 import re
 
+global_total_deletions = 0
+global_removed_files = []
+global_same_files_with_different_name = []
+
+def check_duplicates_in_revision_folders(path_publication, dirname_revisions):
+    for dirname_revision in dirname_revisions:
+        path_revision = os.path.join(path_publication, dirname_revision)
+        print("Accessing dir: {}".format(path_revision))
+        check_for_duplicates([path_revision])
+        print("---------")
 
 def chunk_reader(fobj, chunk_size=1024):
     """Generator that reads a file in chunks of bytes"""
@@ -41,7 +51,7 @@ def check_for_duplicates(paths, hash=hashlib.sha1):
     for path in paths:
         print("Path: {}".format(path))
         for dirpath, dirnames, filenames in os.walk(path):
-            for filename in filenames:
+            for filename in sorted(filenames):
                 full_path = os.path.join(dirpath, filename)
                 try:
                     # if the target is a symlink (soft one), this will
@@ -127,23 +137,46 @@ def check_for_duplicates(paths, hash=hashlib.sha1):
     if files_to_remove and removeDuplicated:
         for file_to_remove in files_to_remove:
             os.remove(file_to_remove)
+            global_removed_files.append(file_to_remove)
+            global global_total_deletions
+            global_total_deletions +=1
 
     if equal_files_with_different_name:
         print("WARNING. There are equal_files_with_different_name: {}".format(len(equal_files_with_different_name)))
         equal_files_pairs = zip(equal_files_with_different_name[0::2], equal_files_with_different_name[1::2])
+        global global_same_files_with_different_name
+        global_same_files_with_different_name.append(equal_files_pairs)
         for file_pairs in equal_files_pairs:
             print("Equal file pair:\n"
                   "  {}\n"
                   "  {}\n".format(file_pairs[0], file_pairs[1]))
 
-if sys.argv[1:]:
-    # check_for_duplicates(sys.argv[1:])
-    for path in sys.argv[1:]:
-        for dirpath, dirnames, filenames in os.walk(path):
-            # Assume path_to_publications folder is the input
-            for dirname in dirnames:
-                print("Accessing dir: {}".format(os.path.join(dirpath, dirname)))
-                check_for_duplicates([os.path.join(dirpath,dirname)])
-                print("---------")
-else:
-    print("Please pass the paths to the publications folder: publications/pub_id/revision_number. The script will remove duplicates only in subfolders revisions_number")
+if __name__ == "__main__":
+    if sys.argv[1:]:
+        for path in sys.argv[1:]:
+            if not os.path.exists(path):
+                raise Exception('path {} does not exist'.format(path))
+            re_dirname_is_a_number = re.compile(r'/[0-9]+(/)?$')
+            s_dirname_is_a_number = re_dirname_is_a_number.search(path)
+            if s_dirname_is_a_number:
+                print('input path is a specific publication folder: {}'.format(s_dirname_is_a_number.group(0)))
+                # Get the revisions of the input publication
+                path_publication = path
+                dirname_revisions = next(os.walk(path_publication))[1]
+                check_duplicates_in_revision_folders(path_publication, dirname_revisions)
+            else:
+                print('input path not a publication, but a parent folder')
+                dirnames_publications = next(os.walk(path))[1]
+                for dirname_publication in dirnames_publications:
+                    path_publication = os.path.join(path, dirname_publication)
+                    dirname_revisions = next(os.walk(path_publication))[1]
+                    check_duplicates_in_revision_folders(path_publication, dirname_revisions)
+        print("Finished!")
+        print("Stats:")
+        print("Total files deleted: {}".format(global_total_deletions))
+        print("Files deleted: {}".format(global_removed_files))
+        print("Equal files with different names:\n{}".format(global_same_files_with_different_name))
+        print("Pairs of equal files with different names: {}.".format(len(global_same_files_with_different_name)))
+    else:
+        print("Please pass the paths to the publications folder: publications/pub_id or a path to the parent folder: publications/"
+              "The script will remove duplicates only in subfolders revisions_number")
