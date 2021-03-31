94af42e21b81ab1006836f2b3e904a93ce692507
Pablo Hernandez-Cerdan <pablo.hernandez.cerdan@outlook.com>
2021-03-31 14:41:42 +0200

    Add publication list to all_issues
    issues will keep a reference to the publications it has, but not the
    other way around. This, is publication/id/metadata.json won't know to
    what issue it belongs.
    
    To get that information a query must be performed to `all_issues`
    
    See https://github.com/InsightSoftwareConsortium/InsightJournal/pull/21#issuecomment-810957056

--- sql-scripts/extract_all_issues.sql
+++ sql-scripts/extract_all_issues.sql
@@ -1,10 +1,29 @@
-/* all issues (collections) */
+/* all issues (collections) with publications */
+with pubcol as (
+  select
+    pub.id,
+    pub.itemid,
+    it.owning_collection
+  from isj_publication pub
+  left join collection2item c2i on c2i.item_id = pub.itemid
+  left join
+    (
+     select
+       item_id,
+       it.owning_collection
+     from
+       item it
+    ) as it on it.item_id = c2i.item_id
+)
 select
 json_build_object(
   'id', cx.collection_id,
   'name', name,
   'short_description', short_description,
-  'introductory_text', introductory_text
+  'introductory_text', introductory_text,
+  'publications', coalesce(jsonb_agg(pubcol.id order by pubcol.id)
+    filter (where pubcol.id is not null), '[]')
 )
 from collection cx
-order by cx.collection_id asc
+left join pubcol on cx.collection_id = pubcol.owning_collection
+group by cx.collection_id
